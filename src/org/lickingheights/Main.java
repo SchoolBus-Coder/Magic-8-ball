aab5fd3a9f240d9f2ff4e4af382cd0e1c8fa67ea
Zac Miller <zahary.miller.2023@student.lhschools.org>
2019-10-03 09:32:10 -0400

    declaring scanner keyboard and question

---
+++ src/org/lickingheights/Main.java
@@ -0,0 +1,21 @@
+package org.lickingheights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//declare phase
+        Scanner Keyboard;
+    int testing;
+    String question;
+
+
+
+
+
+
+
+
+    }
+}
