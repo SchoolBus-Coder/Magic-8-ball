3f59abe88158ee41c1eb027210b2f95c243be84d
Zac Miller <zahary.miller.2023@student.lhschools.org>
2019-10-03 10:14:09 -0400

    initialized 2 good and bad variables

--- src/org/lickingheights/Main.java
+++ src/org/lickingheights/Main.java
@@ -12,6 +12,25 @@ public class Main {
 
 
 
+    //initializing phase
+        Keyboard = new Scanner(System.in);
+        System.out.println("Give me a number 1-100 and a random question.");
+        int firstNumber = Keyboard.nextInt();
+        if (10 >= firstNumber && firstNumber > 0) {
+            System.out.print("Very Possible");
+        }
+        else if (20 >= firstNumber && firstNumber > 11) {
+            System.out.print("Impossible");
+        }
+        else if (30 >= firstNumber && firstNumber > 21) {
+            System.out.print("Yes");
+        }
+        else if (40 >= firstNumber && firstNumber > 31) {
+            System.out.print("That is very unlikely");
+        }
+        
+
+
 
 
 
